4bed73fbe9683f3b00fa156b69c91dedf322f759
kurtreyn <kurtreyn@yahoo.com>
2022-01-30 17:40:55 -0500

    added / path to landing page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import './Signup';
 import Signup from './Signup';
+import Login from './Login';
 
 export default function MainComponent() {
   return (
     <div>
       <Routes>
-        <Route exact path="/login" element={<LandingPage />} />
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
       </Routes>
     </div>
