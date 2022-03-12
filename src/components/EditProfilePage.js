7e34c69af08df3b875314880129abaad7c9d83ec
William Santee <william.santee@gmail.com>
2022-03-12 10:54:53 -0700

    Update EditProfilePage.js
    please add space between imports and functions

--- src/components/EditProfilePage.js
+++ src/components/EditProfilePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { updateProfile } from "firebase/auth";
+
 function EditProfilePage({ currentUser, setCurrentUser }) {
   const storage = getStorage();
   const [loading, setLoading] = useState(false);
